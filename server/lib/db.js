f2ae2b16af8b780df6fd902048a64c8a0fb86f97
Zishan9110 <145319858+Zishan9110@users.noreply.github.com>
2026-02-08 03:13:04 +0530

    update

--- server/lib/db.js
+++ server/lib/db.js
@@ -1,10 +1,30 @@
 import mongoose from "mongoose";
 
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = {
+    conn: null,
+    promise: null,
+  };
+}
+
 export const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGODB_URI);
-    console.log("✅ Database Connected");
-  } catch (error) {
-    console.log("❌ Database Connection Error:", error.message);
+  if (cached.conn) {
+    console.log("✅ Using cached DB connection");
+    return cached.conn;
   }
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(process.env.MONGODB_URI, {
+      bufferCommands: false,
+      maxPoolSize: 10,
+    }).then((mongoose) => {
+      console.log("✅ Database Connected");
+      return mongoose;
+    });
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
 };
